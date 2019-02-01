8849e0a08e60e6aa1bc5cfb8fd1aa7a70b0fba7f
Zahen Malla Osman <zahen.malla-osman@abolis.fr>
2019-02-01 17:28:53 +0100

    Upgrade shared-client to 2.2.0 and fix some style issues

--- projects/app/src/app/app.component.ts
+++ projects/app/src/app/app.component.ts
@@ -3,7 +3,8 @@ import { ToasterConfig } from 'angular2-toaster';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['app.component.scss']
 })
 export class AppComponent {
   public toasterconfig: ToasterConfig = new ToasterConfig({

8cf063fb1f446ec9c79a66762bbc3bf1b64897e0
Zahen Malla Osman <zahen.malla-osman@abolis.fr>
2020-02-12 10:31:40 +0100

    Fix tests

--- cypress/integration/display-plate.spec.ts
+++ cypress/integration/display-plate.spec.ts
@@ -17,7 +17,7 @@ describe('Display plate', () => {
   it('should display a plate by clicking on its barcode', () => {
     checkAllWells('', WHITE_COLOR);
     cy.get('.barcode').contains(BARCODE_1).click();
-    checkAllWells(BARCODE_1.substring(0, 10) + '...');
+    checkAllWells(BARCODE_1.substring(0, 9) + '...');
     mustBeReadonlyPlate();
     cy.get('span.badge span').should('have.text', '20');
     cy.get('.barcode').should('have.length', 20);
@@ -27,14 +27,14 @@ describe('Display plate', () => {
     cy.get('#barcode-filter').clear();
     cy.get('.barcode').should('have.length', 20);
     cy.get('.barcode').contains(BARCODE_2).click();
-    checkAllWells(BARCODE_2.substring(0, 10) + '...');
+    checkAllWells(BARCODE_2.substring(0, 9) + '...');
     mustBeReadonlyPlate();
   });
 
   it('should switch between tabs (create and display) to check that things are reset properly between each', () => {
     checkAllWells('', WHITE_COLOR);
     cy.get('.barcode').contains(BARCODE_1).click();
-    checkAllWells(BARCODE_1.substring(0, 10) + '...');
+    checkAllWells(BARCODE_1.substring(0, 9) + '...');
     cy.get('a').contains('Create').click();
     checkAllWells('', WHITE_COLOR);
     selectAllWells();

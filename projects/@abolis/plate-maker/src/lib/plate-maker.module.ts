9c76b8fd7f697f8559e72ccc8d27c407f9ef92be
Zahen <zahen.malla-osman@abolis.fr>
2023-03-24 11:19:38 +0100

    Fix break changes

--- projects/@abolis/plate-maker/src/lib/plate-maker.module.ts
+++ projects/@abolis/plate-maker/src/lib/plate-maker.module.ts
@@ -26,6 +26,6 @@ export class PlateMakerModule {
 declare module '@angular/core' {
   interface ModuleWithProviders<T = any> {
     ngModule: Type<T>;
-    providers?: Provider[];
+    providers?: (Provider | EnvironmentProviders)[];
   }
 }
